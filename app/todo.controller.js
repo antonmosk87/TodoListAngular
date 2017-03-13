baa646c73da4aac2b8ef85743ca309e870178349
Anton <antonmosk87@gmail.com>
2017-03-13 11:13:26 -0700

    added sort options

--- app/todo.controller.js
+++ app/todo.controller.js
@@ -12,19 +12,32 @@
         var vm = this;
 
 
+        vm.sortField = 'name';
+
         vm.items = [];
 
-        vm.sort = [];
 
-        vm.priorities = [
-            ['High', 3, "alert alert-danger"],
-            ['Medium', 2, "alert alert-warning"],
-            ['Low', 1, "alert alert-info"]
+        vm.priorities = [{
+                type: 'High',
+                number: 3,
+                style: "alert alert-danger"
+            },
+            {
+                type: 'Medium',
+                number: 2,
+                style: "alert alert-warning"
+            },
+            {
+                type: 'Low',
+                number: 1,
+                style: "alert alert-info"
+            }
         ];
 
         vm.addItem = function() {
-            vm.items.push([vm.newItem, vm.priority[1], vm.priority[2]])
+            vm.items.push(vm.newItem)
             vm.newItem = '';
         }
+
     }
 })();
